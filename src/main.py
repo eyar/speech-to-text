015a8d880d781ad85139d893f5b96f7b301c88e5
Eyar <eyar.gilad@gmail.com>
2022-11-27 11:24:01 +0200

    remove main.py

--- src/main.py
+++
@@ -1,5 +0,0 @@
-import jwt
-
-# encoded = jwt.encode({"username": "world"}, "JWT_SECRET", algorithm="HS256")
-# decoded_token = jwt.decode(encoded, "JWT_SECRET", algorithms="HS256")
-print(type({"filename": "aaaaa"}))
